6a5fed833a419fb53fa72f841f1c1438bbb78abb
Mained606 <mained606@gmail.com>
2025-12-09 23:43:14 +0900

    수정
    수정

--- Assets/Learn/Scripts/UI/Common/Tooltip/TooltipData.cs
+++ Assets/Learn/Scripts/UI/Common/Tooltip/TooltipData.cs
@@ -11,7 +11,8 @@ public class TooltipData
     public string description;
     public Sprite icon;
     public List<TooltipStatLine> statLines;
-
+    
+    // 생성자
     public TooltipData(string title = "", string subtitle = "", string description = "", Sprite icon = null, List<TooltipStatLine> statLines = null)
     {
         this.title = title;
